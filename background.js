c5f069043bfffca22fc895b3a4dd044da9b293a9
Enzo Christian Roosch Queiroz <85234531+enzorooschqueiroz@users.noreply.github.com>
2025-07-09 21:02:24 -0300

    :bug: fix: update fetch request for getClientes action to include method and headers

--- background.js
+++ background.js
@@ -1,13 +1,19 @@
 // background.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "getClientes") {
-    fetch("https://api.znit.ai/contas_ativas")
-      .then(res => res.json())
-      .then(data => sendResponse({ success: true, data }))
-      .catch(err => {
-        console.error("Erro no background:", err);
-        sendResponse({ success: false, error: err.message });
-      });
+    fetch("https://api.znit.ai/contas/ativas", {
+      method: "GET",
+      headers: {
+        "Authorization": "q9c8Lk3aN3MtRl1w7a0eFaGYn2P3Ft1DbqW9xQtx6YU", // Substitua pelo seu token real
+        "Content-Type": "application/json"
+      }
+    })
+    .then(res => res.json())
+    .then(data => sendResponse({ success: true, data }))
+    .catch(err => {
+      console.error("Erro no background:", err);
+      sendResponse({ success: false, error: err.message });
+    });
 
     return true; // Mantém o canal aberto para o sendResponse async
   }
